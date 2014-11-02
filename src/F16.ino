8c1be8dea72f8fb8d859c3879847f9deb8bdf1ea
Craig Hollabaugh <craig@hollabaugh.com>
2014-11-02 00:44:06 -0600

    initial add for most first working version

--- src/F16.ino
+++ src/F16.ino
@@ -1,48 +1,297 @@
 #include "lucky7.h"
 #include "RC65X.h"
+#include "RMYD065.h"
 #include "IRremote.h"
+#include <EEPROM.h>
+
+/*
+  see F16 Light Dimming Plot.ods
+  pwm = a+b*exp(d*t/e)
+*/
+
+#define A 275
+#define B -40
+#define D 1
+#define E 500
+
+/*
+// dim settings
+#define A 150
+#define B -40
+#define D 1
+#define E 800
+*/
+
+/*  there's an interrupt collision with the IR routines and the PWM
+  1, 2, 5 work 
+  3, 6, 7 doesn't work 
+*/
+
+enum {
+    LIGHTTHRESHOLDADDRESSH,
+    LIGHTTHRESHOLDADDRESSL
+};
+
+enum {
+    MODE_OVERRIDE = 'O',
+    MODE_DAY = 'D',
+    MODE_EVENING = 'E',
+    MODE_NIGHT = 'N'
+};
 
 Lucky7 hw = Lucky7();
 
-void setup()
-{
-  Serial.begin(115200);
-  Serial.println("NMNSH F-16 Lighting Controller setup");
-  hw.setup();
+#define TIMEOUTSTATUS    500
+#define TIMEOUTEVENING   240000L
+#define TIMEOUTOVERRIDE  30000
+uint32_t timeoutStatus;
+uint32_t timeoutOverride;
+uint32_t timeoutEvening;
+uint16_t lightThreshold;
+
+uint32_t positionUpdateTime;
+uint32_t positionNext;
+
+uint8_t mode;
+
+void allOn() {
+    hw.o1On();
+    hw.o2On();
+    hw.o3On();
+    hw.o4On();
+    hw.o5On();
+    hw.o6On();
+    hw.o7On();
+}
+
+void allOff() {
+    hw.o1Off();
+    hw.o2Off();
+    hw.o3Off();
+    hw.o4Off();
+    hw.o5Off();
+    hw.o6Off();
+    hw.o7Off();
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("NMNSH F-16 Lighting Controller setup");
+    hw.setup();
+    timeoutStatus = 0;
+    lightThreshold = (EEPROM.read(LIGHTTHRESHOLDADDRESSH) << 8) + EEPROM.read(LIGHTTHRESHOLDADDRESSL);
+
+    positionUpdateTime = 0;
+    timeoutOverride = 0;
+    mode = MODE_DAY;
 }
 
 void processKey(uint32_t key) {
-  Serial.print(millis());
-  Serial.print(" ");
-  Serial.println(key,HEX);
-  switch(key) {
-    case RC65X_KEY1:
-      if (hw.o13 == 0) {
-        hw.o13On();
-      } else {
-        hw.o13Off();
+    Serial.print("key ");
+    Serial.println(key, HEX);
+    switch (key) {
+      case '0':
+      case RC65X_KEY0:
+      case RM_YD065_KEY0:
+          allOff();
+          break;
+      case '1':
+      case RC65X_KEY1:
+      case RM_YD065_KEY1:
+          hw.o1Toggle();
+          break;
+      case '2':
+      case RC65X_KEY2:
+      case RM_YD065_KEY2:
+          hw.o2Toggle();
+          break;
+      case '3':
+      case RC65X_KEY3:
+      case RM_YD065_KEY3:
+          hw.o3Toggle();
+          break;
+      case '4':
+      case RC65X_KEY4:
+      case RM_YD065_KEY4:
+          hw.o4Toggle();
+          break;
+      case '5':
+      case RC65X_KEY5:
+      case RM_YD065_KEY5:
+          hw.o5Toggle();
+          break;
+      case '6':
+      case RC65X_KEY6:
+      case RM_YD065_KEY6:
+          hw.o6Toggle();
+          break;
+      case '7':
+      case RC65X_KEY7:
+      case RM_YD065_KEY7:
+          hw.o7Toggle();
+          break;
+      case 'R':
+      case RC65X_KEYTVINPUT:
+      case RM_YD065_KEYINPUT:
+          uint16_t val;
+          val = hw.photocell2();
+          EEPROM.write(LIGHTTHRESHOLDADDRESSH, (val >> 8));
+          EEPROM.write(LIGHTTHRESHOLDADDRESSL, (val & 0xFF));
+          lightThreshold = (EEPROM.read(LIGHTTHRESHOLDADDRESSH) << 8) + EEPROM.read(LIGHTTHRESHOLDADDRESSL);
+          break;
+      case '8':
+      case RC65X_KEY8:
+      case RM_YD065_KEY8:
+          allOn();
+          break;
+      case 'E':
+      case RC65X_KEYPLAY:
+      case RM_YD065_KEYPLAY:
+          Serial.println("PLAY");
+          timeoutEvening = millis() + TIMEOUTEVENING;
+          mode = MODE_EVENING;
+          break;
       }
+}
+
+
+
+void updateLights() {
+    uint32_t now = millis() / 100;
+    uint8_t p;
+
+    if ((now % 20) == 0) {
+        hw.o8On();
+    } else {
+        hw.o8Off();
+    }
+
+    if (mode == MODE_EVENING) {
+        if ((now % 20) == 0) {
+            hw.o13On();
+        } else {
+            hw.o13Off();
+        }
+    } else {
+        hw.o13Off();
+    }
+
+    // no need to hammer this
+    if (millis() > positionUpdateTime) {
+      switch (mode) {
+        case MODE_DAY:
+        case MODE_EVENING:
+          p = A + B * exp(D * float ((millis() % 1000)) / E);
+          hw.o1Set(p);
+          positionUpdateTime = millis() + 10;
+          break;
+        case MODE_NIGHT:
+          allOff();
+          break;
+      }
+    }
+}
+
+void statemap() {
+  switch (mode) {
+    case MODE_OVERRIDE:
+      if (millis() > timeoutOverride) {
+        mode = MODE_DAY;
+      }
+      break;
+    case MODE_DAY:
+        if (hw.photocell2() < lightThreshold) {
+            timeoutEvening = millis() + TIMEOUTEVENING;
+            mode = MODE_EVENING;
+        }
+      break;
+    case MODE_EVENING:
+        if (hw.photocell2() > lightThreshold) {
+            mode = MODE_DAY;
+            return;
+        }
+      if (millis() > timeoutEvening) {
+          timeoutEvening = 0;
+          mode = MODE_NIGHT;
+      }
+      break;
+    case MODE_NIGHT:
+        if (hw.photocell2() > lightThreshold) {
+            mode = MODE_DAY;
+        }
+      break;
   }
 }
 
-void modeLoop() {
-  uint32_t now = millis()/1000;
+void status() {
+    char buffer[40];
+    if (millis() > timeoutStatus) {
+        // Serial.print("\x1B[0;0f\x1B[K"); // home
+        // Serial.print("\x1B[0J"); // clear
 
-  if ((now % 2) == 0) {
-    hw.o3Set(10);
-  } else {
-    hw.o3Off();
-  }
+        timeoutStatus = millis() + TIMEOUTSTATUS;
+        Serial.print("lightLevel:");
+        Serial.print(hw.photocell2());
+        Serial.print(" ");
+        Serial.print("lightThreshold:");
+        Serial.print(lightThreshold);
+        Serial.print(" ");
+        Serial.print("mode:");
+        Serial.print((char)mode);
+
+        sprintf(buffer," lights:%3d %3d %3d %3d %3d %3d %3d",hw.o1,hw.o2,hw.o3,hw.o4,hw.o5,hw.o6,hw.o7);
+        Serial.print(buffer);
+
+
+        if (mode == MODE_EVENING) {
+            Serial.print(" ");
+            Serial.print("timeoutEvening:");
+            Serial.print(timeoutEvening - millis());
+        }
+        if (mode == MODE_OVERRIDE) {
+            Serial.print(" ");
+            Serial.print("timeoutOverride:");
+            Serial.print(timeoutOverride - millis());
+        }
+
+        Serial.println();
+    }
 }
- 
+
+void input() {
+    unsigned long irKey;
+    irKey = hw.loop();
+    if (irKey) {
+        mode = MODE_OVERRIDE;
+        timeoutOverride = millis() + TIMEOUTOVERRIDE;
+        processKey(irKey);
+    }
+
+    if (Serial.available()) {
+        mode = MODE_OVERRIDE;
+        timeoutOverride = millis() + TIMEOUTOVERRIDE;
+        processKey(Serial.read()); 
+    }
+}
+
 
 void loop() {
-  unsigned long irKey;
-  irKey = hw.loop();
+    input();
+    updateLights();
+    statemap();
+    status();
 
-  if (irKey) {
-    processKey(irKey);
-  }
-  modeLoop();
+/*
+    hw.o1MoveTo(255,10);
+    hw.o1MoveTo(0,50);
+    delay(100);
+
+    hw.o5MoveTo(255,100);
+    hw.o5MoveTo(0,2000);
+    delay(100);
+*/
+
 }
 
+/*
+*/
--- src/lucky7.cpp
+++ src/lucky7.cpp
@@ -4,7 +4,6 @@
 IRrecv irrecv(IR);
 decode_results results;
 
-
 void Lucky7::setup() {
   o1 = 0;
   o2 = 0;
@@ -25,11 +24,17 @@ void Lucky7::setup() {
   pinMode(O8,OUTPUT);
   pinMode(O13,OUTPUT);
   timeout = 0;
+
+  pinMode(A4,INPUT);
+  pinMode(A5,INPUT);
+  aveptr = 0;
+
   irrecv.enableIRIn(); // Start the receiver
 }
 
 
 uint32_t Lucky7::loop() {
+  uint8_t i = (aveptr++) % AVECNT;
   uint32_t rv = 0;
   analogWrite(O1,o1);
   analogWrite(O2,o2);
@@ -41,6 +46,9 @@ uint32_t Lucky7::loop() {
   digitalWrite(O8,o8);
   digitalWrite(O13,o13);
 
+  pc1[i] = analogRead(A4);
+  pc2[i] = analogRead(A5);
+
   if (millis() > timeout) {
       if (irrecv.decode(&results)) {
         rv = results.value;
@@ -50,4 +58,155 @@ uint32_t Lucky7::loop() {
       irrecv.resume(); // Receive the next value
     }
   return rv;
+}
+
+uint16_t Lucky7::photocell1() {
+    uint16_t sum = 0;
+    uint8_t i;
+    for (i = 0; i < AVECNT; i++) {
+        sum += pc1[i];
+    }
+    return (uint16_t)(sum/AVECNT);
+}
+
+uint16_t Lucky7::photocell2() {
+    uint32_t sum = 0;
+    uint8_t i;
+    for (i = 0; i < AVECNT; i++) {
+        sum += pc2[i];
+    }
+    return (uint16_t)(sum/AVECNT);
+}
+
+
+// this could be done more efficiently
+void Lucky7::o1MoveTo(uint8_t v, uint16_t stepdelay) {
+    int16_t i;
+    if (v == o1) {
+        return;
+    } 
+    if (v > o1) {
+        for (i = o1; i < v; i++) {
+            //Serial.println(i);
+            analogWrite(O1,i);
+            delayMicroseconds(stepdelay);
+        }
+    } else {
+        for (i = o1; i >= v; i--) {
+            //Serial.println(i);
+            analogWrite(O1,i);
+            delayMicroseconds(stepdelay);
+        }
+    }
+    o1 = v;
+}
+
+// this could be done more efficiently
+void Lucky7::o2MoveTo(uint8_t v, uint16_t stepdelay) {
+    int16_t i;
+    if (v == o2) {
+        return;
+    } 
+    if (v > o2) {
+        for (i = o2; i < v; i++) {
+            //Serial.println(i);
+            analogWrite(O2,i);
+            delayMicroseconds(stepdelay);
+        }
+    } else {
+        for (i = o2; i >= v; i--) {
+            //Serial.println(i);
+            analogWrite(O2,i);
+            delayMicroseconds(stepdelay);
+        }
+    }
+    o2 = v;
+}
+
+// this could be done more efficiently
+void Lucky7::o3MoveTo(uint8_t v, uint16_t stepdelay) {
+    int16_t i;
+    if (v == o3) {
+        return;
+    } 
+    if (v > o3) {
+        for (i = o3; i < v; i++) {
+            //Serial.println(i);
+            analogWrite(O3,i);
+            delayMicroseconds(stepdelay);
+        }
+    } else {
+        for (i = o3; i >= v; i--) {
+            //Serial.println(i);
+            analogWrite(O3,i);
+            delayMicroseconds(stepdelay);
+        }
+    }
+    o3 = v;
+}
+
+// this could be done more efficiently
+void Lucky7::o5MoveTo(uint8_t v, uint16_t stepdelay) {
+    int16_t i;
+    if (v == o5) {
+        return;
+    } 
+    if (v > o5) {
+        for (i = o5; i < v; i++) {
+            //Serial.println(i);
+            analogWrite(O5,i);
+            delayMicroseconds(stepdelay);
+        }
+    } else {
+        for (i = o5; i >= v; i--) {
+            //Serial.println(i);
+            analogWrite(O5,i);
+            delayMicroseconds(stepdelay);
+        }
+    }
+    o5 = v;
+}
+
+// this could be done more efficiently
+void Lucky7::o6MoveTo(uint8_t v, uint16_t stepdelay) {
+    int16_t i;
+    if (v == o6) {
+        return;
+    } 
+    if (v > o6) {
+        for (i = o6; i < v; i++) {
+            //Serial.println(i);
+            analogWrite(O6,i);
+            delayMicroseconds(stepdelay);
+        }
+    } else {
+        for (i = o6; i >= v; i--) {
+            //Serial.println(i);
+            analogWrite(O6,i);
+            delayMicroseconds(stepdelay);
+        }
+    }
+    o6 = v;
+}
+
+// this could be done more efficiently
+void Lucky7::o7MoveTo(uint8_t v, uint16_t stepdelay) {
+    int16_t i;
+    if (v == o7) {
+        return;
+    } 
+    if (v > o7) {
+        for (i = o7; i < v; i++) {
+            //Serial.println(i);
+            analogWrite(O7,i);
+            delayMicroseconds(stepdelay);
+        }
+    } else {
+        for (i = o7; i >= v; i--) {
+            //Serial.println(i);
+            analogWrite(O7,i);
+            delayMicroseconds(stepdelay);
+        }
+    }
+    o7 = v;
 }
